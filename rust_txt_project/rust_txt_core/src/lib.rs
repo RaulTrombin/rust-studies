1f05ed760a53c4cbf5dda725c7c7cdb078dd0ab9
RaulVT <raulvtrombin@gmail.com>
2023-04-13 15:38:44 -0300

    update rust with tests

--- rust_txt_project/rust_txt_core/src/lib.rs
+++ rust_txt_project/rust_txt_core/src/lib.rs
@@ -22,3 +22,44 @@ pub fn read_from_file(file_name: &str) -> Result<String, Error> {
     reader.read_to_string(&mut contents)?;
     Ok(contents)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_append_to_file() {
+        // Test appending to a new file
+        let file_name = "test1.txt";
+        let content = "Hello, world!";
+        let result = append_to_file(file_name, content);
+        assert!(result.is_ok());
+
+        // Test appending to an existing file
+        let content2 = "Second line.";
+        let result2 = append_to_file(file_name, content2);
+        assert!(result2.is_ok());
+
+        // Test appending with invalid file name
+        let invalid_file_name = "/some/invalid/path.txt";
+        let result3 = append_to_file(invalid_file_name, content);
+        assert!(result3.is_err());
+        std::fs::remove_file("test1.txt").unwrap();
+    }
+
+    #[test]
+    fn test_read_from_file() {
+        // Test reading from an existing file
+        let file_name = "test2.txt";
+        let content = "Hello, world!";
+        
+        append_to_file(file_name, content);
+        let result = read_from_file(file_name);
+        
+        assert!(result.is_ok());
+        
+        assert_eq!(result.unwrap(), "Hello, world!\n");
+        std::fs::remove_file("test2.txt").unwrap();
+    }
+}
